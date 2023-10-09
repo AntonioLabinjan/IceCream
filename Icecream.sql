8097bc27f6ff154b1be1118b8e9b208816a17248
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2023-10-09 17:08:17 +0200

    Add files via upload
    Added SQL database

---
+++ Icecream.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IceCream; -- Stvorite bazu podataka pod imenom "IceCream"
+
+USE IceCream; -- Koristite tu bazu podataka
+
+CREATE TABLE ice_cream ( -- Stvorite tablicu "IceCream"
+    icecream_id INT PRIMARY KEY,
+    icecream_name VARCHAR(100),
+    icecream_price FLOAT
+);
+
+DROP DATABASE IceCream
+
